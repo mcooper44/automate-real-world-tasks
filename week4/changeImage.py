f6655a78a186c2e640534f512507530a6cfc2f97
Matt Cooper <m.cooper44@gmail.com>
2021-05-17 12:09:11 -0400

    removed loop count b/c it is not implemented and fixed minor typos

--- week4/changeImage.py
+++ week4/changeImage.py
@@ -24,24 +24,21 @@ def open_and_process_image(the_image, source_p, conv_type='RGB',
     
 
     '''
+
     if not the_image.endswith(new_ext):
         # swap extensions on the file 
         new_n = os.path.splitext(the_image)[0] + new_ext
         # build the new path to save the file
         out_file = f'{save_loc}{new_n}'
-        process_count = 0 # keep count of how many files have been processed
         try:
             with Image.open(f'{source_p}{the_image}') as m:
                 m.convert(conv_type).resize(new_size).save(out_file, new_format)
-                process_count += 1
         except OSError as e1:
             print(f'cannot process or save {the_image}')
             print(f'due to: {e1}')
         except Exception as oe:
             print(f'{the_image} raised {oe}')
-    else:
-        continue
-    print(f'converted {process_count} files to {newsize} size + {new_format}')
+    print(f'converted {the_image} to {new_size} size + {new_format}')
 
 if __name__ == '__main__':
     for infile in os.listdir('test_images/'):
