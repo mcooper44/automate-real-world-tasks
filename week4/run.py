ca58201d993b3692b6806c7d99f8175857aecf1d
Matt Cooper <m.cooper44@gmail.com>
2021-05-20 13:20:42 -0400

    run.py

--- week4/run.py
+++ week4/run.py
@@ -1,53 +1,45 @@
 #!/usr/bin/python3.8
 import os
 import requests
-PRODUCTION_PATH = '~/supplier-data/descriptions/'
+
+PRODUCTION_PATH = 'supplier-data/descriptions/'
 FILE_PATH = 'test_files/'
+URL = 'http://localhost/fruits/' # replace localhost with IP
 
 def parse_text_file(txt_f):
+    '''opens a text file and creates a dictionary
+    that can POSTed to an end point'''
     fields = ['name', 'weight', 'description', 'image_name']
     field_index = 0
     structure = {}
     f_root = txt_f.split('/')[-1].split('.')[0]
     try:
         with open(txt_f, 'r') as text:
-            for line in text:
+            for txt in text:
+                line = txt.rstrip()
                 key = fields[field_index]
                 if key == 'weight':
-                    no_lbs = line.strip('lbs')
+                    no_lbs = line.strip(' lbs')
                     structure[key] = int(no_lbs)
                 else:
                     structure[key] = line.rstrip()
                 field_index +=1
-            structure['image_name'] = '{}.jpg'.format(f_root)
+            structure['image_name'] = '{}.jpeg'.format(f_root)
             return structure
     except Exception as error:
-        print(f'{txt_f} raised {error}')
+        print(error)
 
-def post_json(json_struct):
+def post_json(URL, json_struct):
+    '''posts a dictionary/json to a URL'''
     try:
-        attempt.post(URL, data=json.dumps(json_struct))
-        attempt.raise_for_status()
-    except HTTPError as http_error:
-        print(f'HTTP error was raised: {http_err}')
-        print(f'using {file_path} and {url}')
-    except Exception as other_error:
-        print(f'ERROR: {other_error}')
-    else:
-        print(f'description uploaded')
-
+        r = requests.post(URL, json=json_struct)
+        r.raise_for_status()
+    except Exception as error:
+        print(error)
 
 if __name__ == '__main__':
-    file_structure = []
     # parse the text files and build a json structure of descriptions
     for infile in os.listdir(FILE_PATH):
         if infile.endswith('.txt'):
             data_dictionary = parse_text_file(f'{FILE_PATH}{infile}')
-            file_structure.append(data_dictionary)
-
-    # post the text descriptions to the webserver
-    try:
-        print(file_structure)
-        #post_json(file_structure)
-    except Exception as error:
-        raise error
+            post_json(URL, data_dictionary)
